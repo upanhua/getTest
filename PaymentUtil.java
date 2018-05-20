5beaffba4ebda5c450f882e5a6c839aa2a0a07c4
upanhua <36758989+upanhua@users.noreply.github.com>
2018-05-20 15:58:26 +0800

    去掉无用的注释
    去掉无用的注释

--- PaymentUtil.java
+++ PaymentUtil.java
@@ -198,8 +198,4 @@ public class PaymentUtil {
 		return toHex(md.digest(value));
 
 	}
-
-	// public static void main(String[] args) {
-	// System.out.println(hmacSign("AnnulCard1000043252120080620160450.0http://localhost/SZXpro/callback.asp鏉�4564868265473632445648682654736324511","8UPp0KE8sq73zVP370vko7C39403rtK1YwX40Td6irH216036H27Eb12792t"));
-	// }
 }
